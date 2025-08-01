a7d0ae7793842357037460d3dc1b349e89be5c47
Lulu-gaby <lulugabyxxx@gmail.com>
2025-08-01 13:52:16 +0300

    New code added

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    if b == 0:
-        raise ValueError("На ноль делить нельзя!")
-    return a / b
-
-def mod(a, b):
-    if b == 0:
-        raise ValueError("На ноль делить нельзя!")
-    return a % b
+def count_vowels(s):
+    vowels = 'аеиоуыэюяАЕИОУЫЭЮЯ'
+    count = 0
+    for letter in s:
+        if letter in vowels:
+            count += 1
+    return count
--- test.py
+++ test.py
@@ -1,34 +1,23 @@
-import unittest
-from main import add,subtract, multiply, divide, mod
+import pytest
+from main import count_vowels
 
-class TestMath (unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 5), 7)
-        self.assertEqual(add(3,7), 9)
+def test_all_vowels():
+    assert count_vowels("аеиоуыэюяАЕИОУЫЭЮЯ") == len("аеиоуыэюяАЕИОУЫЭЮЯ")
 
-    def test_subtract(self):
-        self.assertEqual(subtract(7, 4), 3)
-        self.assertEqual(subtract(4, 2), 1)
+def test_no_vowels():
+    assert count_vowels("bcdfghjklmnpqrstvwxyz") == 0
 
-    def test_multiply(self):
-        self.assertEqual(multiply(2, 5), 12)
-        self.assertEqual(multiply(3, 6), 18)
+def test_mixed_string():
+    assert count_vowels("Привет друзья!") == 4
 
-    def test_divide(self):
-        self.assertEqual(divide(4, 2), 3)
-        self.assertEqual(divide(20, 5), 4)
+def test_empty_string():
+    assert count_vowels("") == 0
 
-    def test_divide_by_zero(self):
-        self.assertRaises(ValueError, divide, 6, 0)
-
-    def test_mod(self):
-        self.assertEqual(mod(10, 3), 1)
-        self.assertEqual(mod(25, 7), 4)
-
-    def test_mod_by_zero(self):
-        self.assertRaises(ValueError, mod(10, 0))
-
-
-
-if __name__== '__main__':
-    unittest.main()
+@pytest.mark.parametrize("input_string, expected_count", [
+    ("Учеба", 3),
+    ("Тест", 1),
+    ("1234", 0),
+    ("Поздравляю!", 4),
+])
+def test_parametrized(input_string, expected_count):
+    assert count_vowels(input_string) == expected_count
